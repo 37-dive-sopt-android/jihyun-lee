a5746443420d7b96aa707e224c0dbbbd6a9481b1
Jyunee54 <jyunee54@gmail.com>
2025-11-02 00:00:42 +0900

    [feat/#4] 회원가입 뷰 수정

--- app/src/main/java/com/sopt/dive/ui/feature/signup/SignUpScreen.kt
+++ app/src/main/java/com/sopt/dive/ui/feature/signup/SignUpScreen.kt
@@ -2,17 +2,19 @@ package com.sopt.dive.ui.feature.signup
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.sopt.dive.R
@@ -47,23 +49,33 @@ private fun SignUpScreen(
     onSignUpButtonClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    Column(
-        modifier = modifier
-            .imePadding()
-            .padding(20.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            text = stringResource(R.string.signup_title),
-            style = DiveTheme.typography.heather.semibold_32,
-            color = DiveTheme.colors.gray600,
-            modifier = Modifier.padding(top = 20.dp)
-        )
+    Scaffold (
+        modifier = modifier.imePadding(),
+        topBar = {
+            Text(
+                text = stringResource(R.string.signup_title),
+                style = DiveTheme.typography.heather.semibold_32,
+                color = DiveTheme.colors.gray600,
+                modifier = Modifier
+                    .padding(horizontal = 20.dp, vertical = 40.dp)
+                    .fillMaxWidth(),
+                textAlign = TextAlign.Center
+            )
+        },
+        bottomBar = {
+            DiveBasicButton(
+                onClick = onSignUpButtonClick,
+                text = stringResource(R.string.signup_button),
+                isEnable = uiState.isSignUpEnabled,
+                modifier = Modifier.padding(20.dp)
+            )
+        }
+    ) { innerPadding ->
         Column(
             modifier = Modifier
+                .padding(innerPadding)
                 .verticalScroll(rememberScrollState())
-                .weight(1f)
-                .padding(vertical = 40.dp),
+                .padding(horizontal = 20.dp),
             verticalArrangement = Arrangement.spacedBy(30.dp)
         ) {
             DiveBasicTextField(
@@ -108,11 +120,6 @@ private fun SignUpScreen(
                 textFieldValidType = uiState.mbtiValidType
             )
         }
-        DiveBasicButton(
-            onClick = onSignUpButtonClick,
-            text = stringResource(R.string.signup_button),
-            isEnable = uiState.isSignUpEnabled
-        )
     }
 }
 
