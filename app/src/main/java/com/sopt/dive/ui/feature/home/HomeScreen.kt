71f22c1218bde89f59ee68810b506829ef90a3fa
Jyunee54 <jyunee54@gmail.com>
2025-11-08 00:25:20 +0900

    [feat/#6] Home 뷰모델 수정

--- app/src/main/java/com/sopt/dive/ui/feature/home/HomeScreen.kt
+++ app/src/main/java/com/sopt/dive/ui/feature/home/HomeScreen.kt
@@ -9,10 +9,12 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.sopt.dive.R
 import com.sopt.dive.domain.model.ProfileInfo
@@ -23,7 +25,7 @@ fun HomeRoute(
     modifier: Modifier = Modifier,
     viewModel: HomeViewModel = viewModel()
 ) {
-    val uiState = viewModel.uiState
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
 
     HomeScreen(
         uiState = uiState,
--- app/src/main/java/com/sopt/dive/ui/feature/home/HomeViewModel.kt
+++ app/src/main/java/com/sopt/dive/ui/feature/home/HomeViewModel.kt
@@ -1,16 +1,15 @@
 package com.sopt.dive.ui.feature.home
 
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.sopt.dive.data.local.UserPrefs
 import com.sopt.dive.domain.model.ProfileInfo
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class HomeViewModel: ViewModel() {
-    private var _uiState by mutableStateOf(HomeUiState())
-    val uiState
-        get() = _uiState
+    private var _uiState = MutableStateFlow(HomeUiState())
+    val uiState = _uiState.asStateFlow()
 
     init {
         loadData()
@@ -21,71 +20,12 @@ class HomeViewModel: ViewModel() {
             profileImageUrl = "https://i.pinimg.com/736x/96/37/2d/96372ded13d1e6b17cdf10b4ecb23483.jpg",
             name = UserPrefs.getName() ?: ""
         )
-        val dummyFriendList = listOf(
-            ProfileInfo(
-                profileImageUrl = "https://i.pinimg.com/736x/22/42/9b/22429b5137154dbefb84162d58d3f76b.jpg",
-                name = "이지현",
-                statusMessage = "안녕하세요",
-                music = "COLOR - NCT WISH"
-            ),
-            ProfileInfo(
-                name = "이지현",
-                isBirthday = true
-            ),
-            ProfileInfo(
-                profileImageUrl = "https://i.pinimg.com/736x/e6/17/fa/e617fa85c330cba8abbf56e11c542913.jpg",
-                name = "이지현",
-                music = "Drowning - WOODZ"
-            ),
-            ProfileInfo(
-                profileImageUrl = "https://i.pinimg.com/736x/22/42/9b/22429b5137154dbefb84162d58d3f76b.jpg",
-                name = "이지현",
-                statusMessage = "안녕하세요"
-            ),
-            ProfileInfo(
-                name = "이지현",
-                statusMessage = "생일이다아",
-                isBirthday = true
-            ),
-            ProfileInfo(
-                profileImageUrl = "https://i.pinimg.com/736x/e6/17/fa/e617fa85c330cba8abbf56e11c542913.jpg",
-                name = "이지현",
-                music = "FAMOUS - ALLDAY PROJECT"
-            ),
-            ProfileInfo(
-                profileImageUrl = "https://i.pinimg.com/736x/22/42/9b/22429b5137154dbefb84162d58d3f76b.jpg",
-                name = "이지현",
-                statusMessage = "안녕하세요",
-            ),
-            ProfileInfo(
-                name = "이지현",
-                isBirthday = true
-            ),
-            ProfileInfo(
-                profileImageUrl = "https://i.pinimg.com/736x/e6/17/fa/e617fa85c330cba8abbf56e11c542913.jpg",
-                name = "이지현",
-                isBirthday = true,
-                music = "Drowning - WOODZ"
-            ),
-            ProfileInfo(
-                profileImageUrl = "https://i.pinimg.com/736x/22/42/9b/22429b5137154dbefb84162d58d3f76b.jpg",
-                name = "이지현",
-                statusMessage = "안녕하세요",
-                music = "Blue Valentine - NMIXX"
-            ),
-            ProfileInfo(
-                name = "이지현",
-                isBirthday = true
-            ),
-            ProfileInfo(
-                profileImageUrl = "https://i.pinimg.com/736x/e6/17/fa/e617fa85c330cba8abbf56e11c542913.jpg",
-                name = "이지현"
-            )
-        )
 
-        _uiState = _uiState.copy(
-            userProfile = dummyUserProfile,
-            friendList = dummyFriendList
-        )
+        _uiState.update { currentState ->
+            currentState.copy(
+                userProfile = dummyUserProfile,
+                friendList = dummyFriendList
+            )
+        }
     }
 }
